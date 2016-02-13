4fcb9b330be43cc62611708c81a2f4bb73b70cad
luke szweda.luke@gmail.com <luke@MacBook-Pro-luke.local>
2016-02-14 00:21:22 +0100

    catenate and decatenate methods implementation

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -80,8 +80,11 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A.tail == null)
+            return A.tail = B;
+        else
+            dcatenate(A.tail,B);
+        return A;
     }
 
     /**
@@ -89,8 +92,10 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A.tail == null)
+            return new IntList(A.head, B);
+        else
+            return new IntList(A.head, catenate(A.tail, B));
     }
 
 
