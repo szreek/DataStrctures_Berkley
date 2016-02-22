e2c0234d40fceae486d9f2a9be836e25960ecd70
luke szweda.luke@gmail.com <luke@MacBook-Pro-luke.local>
2016-02-22 23:27:13 +0100

    readRadius method refactoring

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,28 +1,36 @@
 public class NBody {
 
 	private static final int RADIUS_POSITION = 1;
-	private static In instream;
 
 	/**
 	* A method that reads universe radius from path
 	* to planets data file
 	**/
 	public static double readRadius(String path){
-		String[] lines =  readLines(path);
-		if (lines[RADIUS_POSITION] == null) throw new IllegalStateException("There is no radius in planets data file");		
-		return Double.parseDouble(lines[RADIUS_POSITION]);
+		double uniRadius = 0.0;
+		In stream = openStream(path);
+		for (int i = 1; i < 3; i++) {
+			if (i % 2 != 0) {
+				stream.readInt();
+			} else {
+				uniRadius = stream.readDouble();
+			}
+		}
+		return uniRadius;
 	}
 
 
+	public static Planet[] readPlanets(String path){
+		return null;
+	}
 	
 	//#################_private_helper_methods_######################
-	private static String[] readLines(String path){
-		String[] lines = null;
+	private static In openStream(String path){
 		In instream = new In(path);
-		if (instream.exists()) {
-			lines = instream.readAllLines();
+		if (!instream.exists()) {
+			throw new IllegalArgumentException("No stream with the given path: " + path);
 		}
-		return lines;
+		return instream;
 	}
 
 }
